f55486f851bc9021700612e7bbe157f242d4c90d
Manidep Gupta <manideep.gupta@gmail.com>
2021-07-18 18:47:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.speechtotext;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView speech;
+    Button speak;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        speech=findViewById(R.id.speech);
+        speak=findViewById(R.id.speak);
+
+        speak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSpeech(v);
+            }
+        });
+
+
+    }
+    public void btnSpeech(View v){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Hi Speak Something");
+
+        try {
+            startActivityForResult(intent, 1);
+        }
+        catch (ActivityNotFoundException e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case 1:
+                if(resultCode==RESULT_OK && data!=null){
+                    ArrayList<String> result =
+                            data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    speech.setText(result.get(0));
+                }
+                break;
+        }
+
+    }
+}
